b3a725e8300dfdfe0e55e81da08776e61d6fa3ef
Michael RP <mikaelphang@gmail.com>
2019-05-04 20:53:39 +0700

    customize delimiter

--- csv_record_view.py
+++ csv_record_view.py
@@ -4,7 +4,17 @@ import re
 
 class CsvrecordviewCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
-		r = re.compile(r'''\s*([^,"']*?|"(?:[^"\\]|\\.)*"|'(?:[^'\\]|\\.)*')\s*(?:,|$)''', re.UNICODE)
+		settings = sublime.load_settings("csv_record_view.sublime-settings")
+		delimiter = settings.get('csvrecordview_delimiter')
+		if delimiter is None:
+			delimiter = self.view.settings().get('csvrecordview_delimiter')
+		if delimiter is None:
+			delimiter = ','
+
+		regex_string = r'''\s*([^${delimiter}"']*?|"(?:[^"\\]|\\.)*"|'(?:[^'\\]|\\.)*')\s*(?:${delimiter}|$)'''
+		regex_string = regex_string.replace(r'${delimiter}', delimiter)
+
+		r = re.compile(regex_string, re.UNICODE)
 		for cursor in self.view.sel():
 			for line in self.view.lines(cursor):
 				text = self.view.substr(line)
